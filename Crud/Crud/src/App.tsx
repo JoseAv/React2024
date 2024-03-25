e56a4194fd4f6c80386a1c43415ce9647138bdca
Jose Arana <jm.aranav@gmail.com>
2024-03-25 17:03:25 -0600

    React Crud, Falta editar y eliminar

--- Crud/Crud/src/App.tsx
+++ Crud/Crud/src/App.tsx
@@ -1,62 +1,83 @@
-import { useState,ChangeEvent  } from 'react'
+import { useState, ChangeEvent, FormEvent } from 'react'
 import './App.css'
+import ShowDatos from './components' 
 
-type typedatos= {
-  name:string,
-  apellido:string
+export  type typedatos = {
+  uid:`${string}-${string}-${string}-${string}-${string}`,
+  name: string,
+  apellido: string
 }
 
 
-function App():JSX.Element {
-  const [datos,setdatos]= useState<typedatos>({name:'',apellido:''})
+function App(): JSX.Element {
+  const [datos, setdatos] = useState<typedatos>({uid:crypto.randomUUID(), name: '', apellido: '' })
   const [personas, setPersonas] = useState<typedatos[]>([]);
-
-const Cambio =(e:ChangeEvent<HTMLInputElement>):void => {
-  
-  setdatos({...datos,
-  [e.target.id]: e.target.value})
-    
-    
+  const Cambio = (e: ChangeEvent<HTMLInputElement>): void => {
+    const {id, value} = e.target
+    setdatos({...datos,
+      uid:crypto.randomUUID(),
+      [id]:value
+    })
 
   }
 
- const Estados: React.FC<React.FormEvent> = (e) => { //React.FC Manjeador de estados
-  setPersonas([...personas,datos])   
-  console.log(datos)                                       // <React> Indicamos que tipo de estado es
-  e.preventDefault();
+  const Estados = (e: FormEvent<HTMLFormElement>): void => { //React.FC Manjeador de estados                                      // <React> Indicamos que tipo de estado es
+    e.preventDefault();
+  };
+
+
+ function CreateEelement(){
+  setPersonas([...personas, datos])
+  setdatos({ uid: crypto.randomUUID(), name: '', apellido: '' })
+
+ }
+
 
   return (
-  <>
-  </>
-  );
-};
+
+    <>
+      <h1>Crud React</h1>
+      <form onSubmit={Estados}>
+        <table>
+        
+          <thead>
+            <tr>
+            
+              <td >
+              <label htmlFor="name"  >Nombre:</label>
+              <input type="text" id='name' value={datos.name} onChange={Cambio}  />
+              </td>
+
+              <td>
+              <label htmlFor="Apellido">Apellido:</label>
+              <input type="text" id='apellido' value={datos.apellido} onChange={Cambio} />
+              </td>
+
+              <td>
+                <input type="Submit" onClick={CreateEelement} value='Crear' className='botom' />
+              </td>
+            </tr>
 
 
-
-  return (
-    
-     <>
-    <h1>Crud React</h1>
-    <form onSubmit={Estados}>
-  <table>
-    <thead>
-    <tr>
+            
+          </thead>
+          
       
-      <td><label htmlFor="name">Nombre:</label><input type="text" id='name' value={datos.name} onChange={Cambio}/></td>
-      <td><label htmlFor="Apellido">Apellido:</label><input type="text" id='apellido' value={datos.apellido} onChange={Cambio}/></td>
-      <td><button className='botom' type='submit'>Agregar</button></td>
-      
-    </tr>
-  e
-  
-  </thead>
 
-  <tbody>
- 
-  </tbody>
-  </table>
-  </form>
-      </>
+
+        </table>
+        </form>
+
+        {personas.map(e=>{
+
+          return(<ShowDatos name={e.name} apellido={e.apellido} uid={e.uid} key={e.uid} />)
+
+        })
+        
+        } 
+
+
+    </>
   )
 }
 
